7a84d3abcfd74235e17c51259e1f380ee39ea838
DurgaKc <kcdurga732@gmail.com>
2025-11-27 14:48:00 +0545

    pending..

--- server/config/connectionDb.js
+++ server/config/connectionDb.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const connectDb = async()=>{
-    await mongoose.connect(process.env.CONNECTION_STRING)
+    await mongoose.connect(process.env.MONGO_URI)
     .then(()=>console.log("Connected Successfully!"))
 }
 export default connectDb;
